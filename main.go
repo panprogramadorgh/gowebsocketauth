b292184fbc5917f3076b0aacd6cfb87798fddfb8
AlvaroBT7 <barreroalvaro2007@gmai.com>
2024-06-10 00:29:49 +0200

    Auth system debugged

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+/* ------------------------------------------------------ */
+
 type User struct {
 	Username string
 	Password string
@@ -21,10 +23,17 @@ type Session struct {
 	Conn *websocket.Conn
 }
 
+/* ------------------------------------------------------ */
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
+
+/* ------------------------------------------------------ */
 
 /*
 `clients` Stores each client connection
@@ -32,7 +41,7 @@ NOTE: It's important to remove the client connection once it's closed. It is als
 */
 
 var clients []*websocket.Conn
-var sessions []Session
+var sessions []*Session
 var users []User = []User{
 	{
 		Username: "alvaro",
@@ -44,13 +53,11 @@ var users []User = []User{
 	},
 }
 
-/*
-------------- FIXME: Arreglaer funciones close ---------------
-*/
+/* ------------------------------------------------------ */
 
 func closeSession(session *Session) error {
 	for i, eachSession := range sessions {
-		if &eachSession == session {
+		if eachSession == session {
 			// Removing session from slice
 			sessions = append(sessions[:i], sessions[i+1:]...)
 			return nil
@@ -63,17 +70,25 @@ func closeSession(session *Session) error {
 closeClient function first removes the session then disconnects the client
 */
 func closeClient(clientConn *websocket.Conn) error {
-	// Closing all the sessions of the client
-	for i, eachSession := range sessions {
-		if eachSession.Conn == clientConn {
-			// Closing the connection
-			eachSession.Conn.Close()
-			// Removing session from slice
-			sessions = append(sessions[:i], sessions[i+1:]...)
-			return nil
+	// Removes the client conn from the client slice
+	for i, eachCliConn := range clients {
+		if eachCliConn == clientConn {
+			clients = append(clients[:i], clients[i+1:]...)
+		} else if len(clients)-1 == i {
+			// Executes if it is the last cicle and the connection wasn't found
+			return errors.New("unknown client connection")
 		}
 	}
-	return errors.New("error when closing client connection")
+	// Closes any session asociated to the connection
+	cliSession := findSessionPerConn(clientConn)
+	if cliSession != nil {
+		if err := closeSession(cliSession); err != nil {
+			return err
+		}
+	}
+	// Closes the connection
+	(*clientConn).Close()
+	return nil
 }
 
 /* ------------------------------------------------------ */
@@ -81,7 +96,16 @@ func closeClient(clientConn *websocket.Conn) error {
 func findSessionPerConn(cliConn *websocket.Conn) *Session {
 	for _, eachSession := range sessions {
 		if eachSession.Conn == cliConn {
-			return &eachSession
+			return eachSession
+		}
+	}
+	return nil
+}
+
+func findSessionPerUsrname(usrname string) *Session {
+	for _, eachSession := range sessions {
+		if eachSession.User.Username == usrname {
+			return eachSession
 		}
 	}
 	return nil
@@ -113,10 +137,8 @@ func main() {
 		// When the handler function ends the connection it's been close
 		defer func() {
 			if err := closeClient(conn); err != nil {
-				if err := conn.WriteMessage(websocket.TextMessage, []byte(err.Error())); err != nil {
-					fmt.Println(err)
-					return
-				}
+				fmt.Println(err)
+				return
 			}
 		}()
 
@@ -124,8 +146,9 @@ func main() {
 			// Reads messages from any client
 			_, msg, err := conn.ReadMessage()
 			if err != nil {
+				// In case of the client closes the connection, the infinite loop will stop and the deffered function will execute removing the client session (if the cli has any session)
 				fmt.Println(err)
-				return
+				break
 			}
 
 			message := string(msg)
@@ -136,12 +159,7 @@ func main() {
 
 				if command == "exit" {
 					/* Command used for closing the session and the connection */
-					if err := closeClient(conn); err != nil {
-						if err := conn.WriteMessage(websocket.TextMessage, []byte(err.Error())); err != nil {
-							fmt.Println(err)
-							break
-						}
-					}
+					break
 				} else if command == "list" {
 					/* Command used for showing all the client connections */
 
@@ -178,34 +196,45 @@ func main() {
 						usrname := args[1]
 						password := args[2]
 
-						// Checks if the user exists
-						auth := authenticate(usrname, password)
-
-						// Adds a new session if auth
-						if auth {
-							sessions = append(sessions, Session{Conn: conn, User: User{Username: usrname, Password: password}})
-							if err := conn.WriteMessage(websocket.TextMessage, []byte("Logged as "+usrname)); err != nil {
+						sessionIsActive := findSessionPerUsrname(usrname)
+						if sessionIsActive != nil {
+							if err := conn.WriteMessage(websocket.TextMessage, []byte("Session already active")); err != nil {
 								fmt.Println(err)
-								break
-							}
-							// Warns all the clients that a new client has joined
-							var errWhenWritingMsg error = nil
-							for _, client := range clients {
-								if err := client.WriteMessage(websocket.TextMessage, []byte(usrname+" joined to the server")); err != nil {
-									fmt.Println(err)
-									errWhenWritingMsg = err
-								}
-							}
-							// Closes all connections with users in case there was an error sending to each user the joining message
-							if errWhenWritingMsg != nil {
 								break
 							}
 						} else {
-							if err := conn.WriteMessage(websocket.TextMessage, []byte("Invalid credentials")); err != nil {
-								fmt.Println(err)
-								break
+							// Checks if the user exists
+							auth := authenticate(usrname, password)
+
+							// Adds a new session if auth
+							if auth {
+								newSession := &Session{Conn: conn, User: User{Username: usrname, Password: password}}
+								sessions = append(sessions, newSession)
+								if err := conn.WriteMessage(websocket.TextMessage, []byte("Logged as "+usrname)); err != nil {
+									fmt.Println(err)
+									break
+								}
+								// Warns all the clients that a new client has joined
+								var errWhenWritingMsg error = nil
+								for _, client := range clients {
+									if err := client.WriteMessage(websocket.TextMessage, []byte(usrname+" joined to the server")); err != nil {
+										fmt.Println(err)
+										errWhenWritingMsg = err
+									}
+								}
+								// Closes all connections with clients in case there was an error sending to each client the joining message
+								if errWhenWritingMsg != nil {
+									break
+								}
+							} else {
+								if err := conn.WriteMessage(websocket.TextMessage, []byte("Invalid credentials")); err != nil {
+									fmt.Println(err)
+									break
+								}
 							}
+
 						}
+
 					}
 				} else if command == "logout" {
 					currentSession := findSessionPerConn(conn)
@@ -217,6 +246,18 @@ func main() {
 						}
 					} else {
 						// Removes the session
+						var errWhenWritingMsg error = nil
+						for _, cliConn := range clients {
+							if err := cliConn.WriteMessage(websocket.TextMessage, []byte(currentSession.User.Username+" left the server")); err != nil {
+								fmt.Println(err)
+								errWhenWritingMsg = err
+							}
+						}
+						// Closes all connections with clients in case there was an error sending to each client the joining message
+						if errWhenWritingMsg != nil {
+							break
+						}
+
 						if err := closeSession(currentSession); err != nil {
 							if err := conn.WriteMessage(websocket.TextMessage, []byte(err.Error())); err != nil {
 								fmt.Println(err)
@@ -243,7 +284,7 @@ func main() {
 				}
 			} else {
 				// Cheks if the client connection has a session
-				var currentSession *Session = findSessionPerConn(conn)
+				currentSession := findSessionPerConn(conn)
 				if currentSession == nil {
 					// Reject the command
 					if err := conn.WriteMessage(websocket.TextMessage, []byte("You are not logged in")); err != nil {
